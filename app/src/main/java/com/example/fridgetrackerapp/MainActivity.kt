6ccd1e8b502c92706128cc600d5a679bf16da7f9
Collin <88743846+PaizCollin@users.noreply.github.com>
2023-02-01 15:54:37 -0800

    Adds top bar and nav drawer to ui

--- app/src/main/java/com/example/fridgetrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/fridgetrackerapp/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.fridgetrackerapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.fridgetrackerapp.ui.theme.FridgeTrackerAppTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,8 +31,67 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Preview(showBackground = true)
 @Composable
 fun FridgeTrackerApp() {
-    
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+    val contextForToast = LocalContext.current.applicationContext
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            TopBar {
+                // open the drawer
+                coroutineScope.launch {
+                    scaffoldState.drawerState.open()
+                }
+            }
+        },
+        drawerContent = { NavDrawer(coroutineScope = coroutineScope, scaffoldState = scaffoldState) }
+    ) {
+
+    }
+}
+
+@Composable
+fun TopBar(onNavigationIconClick: () -> Unit) {
+    TopAppBar(title = {
+        Text(
+            text = "Fridge Tracker",
+            style = MaterialTheme.typography.h6,
+            textAlign = TextAlign.Center
+        )
+    }, navigationIcon = {
+        IconButton(onClick = {
+            onNavigationIconClick()
+        }) {
+            Icon(
+                imageVector = Icons.Outlined.Menu, contentDescription = "navigation"
+            )
+        }
+    })
+}
+
+@Composable
+fun NavDrawer(coroutineScope: CoroutineScope, scaffoldState: ScaffoldState) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // nav drawer text
+        Text(text = "nav")
+
+        // gap between text and button
+        Spacer(modifier = Modifier.height(height = 32.dp))
+
+        // button to close
+        Button(onClick = {
+            coroutineScope.launch { scaffoldState.drawerState.close() }
+        }) {
+            Text(text = "Close")
+        }
+
+    }
 }
